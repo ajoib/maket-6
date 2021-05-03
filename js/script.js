3a4fefeb9e350379653b0344614c8a8606ea55d8
web-rumos <77057949+web-accaunt@users.noreply.github.com>
2021-05-03 19:12:37 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function() { 
+    $('.open_icon').click(function(){ 
+        $('.menu_ul').toggleClass('active');
+     });
+ })
+ 
+ $(function() { 
+    $('.close_icon').click(function(){ 
+        $('.menu_ul').toggleClass('active');
+     });
+ })
